9edd284d8e4e7a8d7cd30f0eb3ecf99a035a48c0
M4XK8R <kpopmyhope@gmail.com>
2023-07-16 15:44:28 +0300

    migrated to one activity app

--- app/src/main/java/com/example/shoppinglist_myown_v2/presentation/rv/ShopItemViewHolder.kt
+++ app/src/main/java/com/example/shoppinglist_myown_v2/presentation/rv/ShopItemViewHolder.kt
@@ -1,16 +1,7 @@
 package com.example.shoppinglist_myown_v2.presentation.rv
 
-import android.view.View
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.shoppinglist_myown_v2.R
+import com.example.shoppinglist_myown_v2.databinding.ViewHolderItemShopEnabledBinding
 
-class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val tvName: TextView
-    val tvCount: TextView
-
-    init {
-        tvName = view.findViewById(R.id.tvName)
-        tvCount = view.findViewById(R.id.tvCount)
-    }
-}
+class ShopItemViewHolder(val binding: ViewHolderItemShopEnabledBinding) :
+    RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/example/shoppinglist_myown_v2/presentation/rv/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist_myown_v2/presentation/rv/ShopListAdapter.kt
@@ -1,10 +1,9 @@
 package com.example.shoppinglist_myown_v2.presentation.rv
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.shoppinglist_myown_v2.R
+import com.example.shoppinglist_myown_v2.databinding.ViewHolderItemShopEnabledBinding
 import com.example.shoppinglist_myown_v2.domain.ShopItem
 
 class ShopListAdapter : RecyclerView.Adapter<ShopItemViewHolder>() {
@@ -12,15 +11,17 @@ class ShopListAdapter : RecyclerView.Adapter<ShopItemViewHolder>() {
     var listShopItem = listOf<ShopItem>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.view_holder_item_shop_enabled, parent, false)
-        return ShopItemViewHolder(view)
+        val binding = ViewHolderItemShopEnabledBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return ShopItemViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
         val shopItem = listShopItem[position]
-        holder.tvName.text = shopItem.name
-        holder.tvCount.text = shopItem.count.toString()
+        holder.binding.apply {
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+        }
     }
 
     override fun getItemCount(): Int = listShopItem.size
