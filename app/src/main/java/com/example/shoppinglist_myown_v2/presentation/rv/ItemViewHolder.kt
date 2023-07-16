39b549c500ffeb6aad11795fbbee8a7a1809b13f
M4XK8R <kpopmyhope@gmail.com>
2023-07-16 12:07:00 +0300

    rollback

--- app/src/main/java/com/example/shoppinglist_myown_v2/presentation/rv/ItemViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.example.shoppinglist_myown_v2.presentation.rv
-
-import androidx.recyclerview.widget.RecyclerView
-import com.example.shoppinglist_myown_v2.databinding.ItemShopDisabledBinding
-
-
-class ItemViewHolder(val binding: ItemShopDisabledBinding) :
-    RecyclerView.ViewHolder(binding.root)
